8a9bf10d97b2c3c359742ab3c02a0329371d83bc
mehrad mon <mehrad.mon2000@yahoo.com>
2022-10-16 14:14:24 -0400

    edited lab1.py file to add a function

--- lab1.py
+++ lab1.py
@@ -4,6 +4,11 @@ def age()
     age= input('What is your age? ')
     print('You are ' + age + ' years old')
     
-    
+def helloWord():
+    print('Hello World')
+
+
 if '__name__' == '__main__':
     age()
+    helloWorld()
+
